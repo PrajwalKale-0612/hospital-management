438f92723111b18a60be6d8038e32b6d30409c69
PrajwalKale-0612 <kaleprajwal0612@gmail.com>
2026-03-27 11:14:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './App.css';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
 import HospitalDashboard from './components/HospitalDashboard';
-
 import Patient from './components/Patient';
 import Doctor from './components/Doctor';
-// import PatientList from "./components/PatientList";
 import PatientDetails from "./components/PatientDetails";
 import AppointmentForm from './components/AppointmentForm';
 
-import { BrowserRouter as Router, Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
-
+import { Routes, Route, BrowserRouter, useLocation } from "react-router-dom";
 
 function Layout() {
   const location = useLocation();
-  const hideNavbar = location.pathname === "/"; // login page ला navbar नको
+  const hideNavbar = location.pathname === "/";
 
   return (
     <>
@@ -26,43 +22,22 @@ function Layout() {
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<HospitalDashboard />} />
 
-        <Route path="/dashboard//patient" element={<Patient />} />
-        {/* <Route index element={<PatientList patients={patients} />} />
-          <Route path="/dashboard/patient/:patientName" element={<PatientDetail patients={patients} />} /> */}
-
-        <Route path="/" element={<Patient />} />
-        {/* <Route path="/patient/:name" element={<PatientDetails />} /> */}
-        {/* <Route path="/dashboard//patient/:name" element={<Patient />} /> */}
-
-        <Route path="/patient" element={<Patient />} />
-        <Route path="/doctor" element={<Doctor />} />
-        <Route path="/dashboard//appointmentform" element={<AppointmentForm />} />
         <Route path="/patient" element={<Patient />} />
         <Route path="/patient/:id" element={<PatientDetails />} />
+
+        <Route path="/doctor" element={<Doctor />} />
+        <Route path="/appointmentform" element={<AppointmentForm />} />
       </Routes>
     </>
   );
 }
 
-
-
 function App() {
   return (
-    <>
-
-      <BrowserRouter>
-        <Layout />
-      </BrowserRouter>
-
-      {/* <Router>
-      <Routes>
-        <Route path="/" element={<Patient1 />} />
-        <Route path="/patient1/:name" element={<PatientDetail />} />
-      </Routes>
-    </Router> */}
-
-    </>
-  )
-};
+    <BrowserRouter>
+      <Layout />
+    </BrowserRouter>
+  );
+}
 
 export default App;
