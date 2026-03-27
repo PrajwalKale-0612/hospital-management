e0b86aefe7010d09862f8c9a24103ca92cfb135f
PrajwalKale-0612 <kaleprajwal0612@gmail.com>
2026-03-27 11:16:06 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,42 @@
 import React from 'react';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import "./Navbar.css";
-class Navbar extends React.Component {
-    state = {}
-    render() {
-        return (
-            <>
-            
-            <h4 className="fw-bold m-0 text-light" >Hospital Management Dashboard    <p className='admin text-light'>Welcome, Admin<i className="fas fa-user-tie text-warning"></i></p></h4>
+
+function Navbar() {
+    return (
+        <>
+            <h4 className="fw-bold m-0 text-light">
+                Hospital Management Dashboard
+                <p className='admin text-light'>
+                    Welcome, Admin <i className="fas fa-user-tie text-warning"></i>
+                </p>
+            </h4>
+
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link to="/dashboard" className="nav-link">Home</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/patient" className="nav-link">Patient</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/doctor" className="nav-link">Doctor</Link>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    Appointments
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    Billing
-                                </a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">
-                                    Reports
-                                </a>
-                            </li>   
-                        </ul> 
-                    </div>
+                    <ul className="navbar-nav me-auto">
+
+                        <li>
+                            <Link to="/dashboard" className="nav-link">Home</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/patient" className="nav-link">Patient</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/doctor" className="nav-link">Doctor</Link>
+                        </li>
+
+                        <li>
+                            <Link to="/appointmentform" className="nav-link">Appointments</Link>
+                        </li>
+
+                    </ul>
                 </div>
             </nav>
-            </>
-        );
-    }
+        </>
+    );
 }
 
 export default Navbar;
