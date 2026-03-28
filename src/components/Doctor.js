8908cde7fe9b507ead126a3f3937817cc2f20422
PrajwalKale-0612 <kaleprajwal0612@gmail.com>
2026-03-28 11:11:48 +0530

    Update Doctor.js

--- src/components/Doctor.js
+++ src/components/Doctor.js
@@ -1,25 +1,53 @@
+
+
+
+
+
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Doctor() {
     return (
-        <div className="container mt-4">
-            <h3>Doctor List</h3>
+        <div className="card-body">
+            <ul className="list-group list-group-flush">
 
-            <ul className="list-group">
-                <li className="list-group-item">
-                    <b>Dr. Priya Desai</b> - Pediatrician
+                <li className="list-group-item bg-warning-subtle">
+                    <p><strong>Name:</strong><i className="fas fa-user-md text-primary"></i> Dr. Priya Desai</p>
+                    <p><strong>Age:</strong> 38</p>
+                    <p><strong>Gender:</strong> Female</p>
+                    <p><strong>Specialist:</strong> Pediatrician</p>
+                    <p><strong>Today's Appointments:</strong> 24</p>
                 </li>
-                <li className="list-group-item">
-                    <b>Dr. A. Kumar</b> - Cardiologist
+
+                <br /><br />
+                <li className="list-group-item bg-warning-subtle">
+                    <p><strong>Name:</strong><i className="fas fa-user-md text-primary"></i> Dr. A. Kumar</p>
+                    <p><strong>Age:</strong> 3</p>
+                    <p><strong>Gender:</strong> Male</p>
+                    <p><strong>Specialist:</strong> Cardiologist</p>
+                    <p><strong>Today's Appointments:</strong> 08</p>
                 </li>
-                <li className="list-group-item">
-                    <b>Dr. R. Nair</b> - Neurologist
+                <br /><br />
+
+                <li className="list-group-item bg-warning-subtle">
+                    <p><strong>Name:</strong><i className="fas fa-user-md text-primary"></i> Dr. R. Nair</p>
+                    <p><strong>Age:</strong> 48</p>
+                    <p><strong>Gender:</strong> Male</p>
+                    <p><strong>Specialist:</strong> Neurologist</p>
+                    <p><strong>Today's Appointments:</strong> 14</p>
                 </li>
-                <li className="list-group-item">
-                    <b>Dr. S. Verma</b> - Dermatologist
+                <br /><br />
+
+                <li className="list-group-item bg-warning-subtle">
+                    <p><strong>Name:</strong><i className="fas fa-user-md text-primary"></i> Dr. S. Verma</p>
+                    <p><strong>Age:</strong> 29</p>
+                    <p><strong>Gender:</strong> Female</p>
+                    <p><strong>Specialist:</strong> Dermatologist</p>
+                    <p><strong>Today's Appointments:</strong> 07</p>
                 </li>
             </ul>
         </div>
-    );
-}
+
+
+    )
+};
